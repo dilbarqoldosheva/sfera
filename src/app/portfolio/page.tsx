2e9f4a3c0649ce21cd2b2541b06ad201ea156f10
dilbarqoldosheva <bintushokir07@gmail.com>
2025-04-17 14:29:39 +0500

    edit

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,9 @@
 const Portfolio = () => {
     return (
         <div>
-            portfolio
+            <h1>Portfolio</h1>
+            <p>Welcome to my portfolio page!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse quia consectetur consequatur. Ullam quos iure deserunt quo saepe voluptatem quisquam cumque dolor, unde, impedit culpa tempore quae temporibus qui alias veniam ab corporis, perferendis dolores error! Necessitatibus, inventore odio nostrum neque cumque sed, repudiandae voluptatum, atque commodi asperiores maxime numquam.</p>
         </div>
     );
 };
