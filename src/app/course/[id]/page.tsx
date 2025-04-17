1ea5301930fc0bdb23b70cf9bb52ff3fceeb951c
dilbarqoldosheva <bintushokir07@gmail.com>
2025-04-17 16:45:39 +0500

    edit

--- src/app/course/[id]/page.tsx
+++ src/app/course/[id]/page.tsx
@@ -29,7 +29,7 @@ export default async function CoursePage({ params }: any) {
 
         <div className="">
             <Hero course={courseData} />
-            <CourseInfoSection  />
+            {/* <CourseInfoSection  /> */}
             <Info />
             <Teachers course={courseData}/>
             <FAQSection />
